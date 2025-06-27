8a658f31ef2438da9535f3d63c587285202bae51
Val'VR <valvrpro@gmail.com>
2025-06-27 15:14:37 +0200

    Use a keyvault instead of hardcoded password

---
+++ kv.tf
@@ -0,0 +1,62 @@
+resource "azurerm_key_vault" "terraform_kv" {
+  name                = "iisnginxterraformkv"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku_name  = "standard"
+  tenant_id = data.azurerm_client_config.current_client.tenant_id
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current_client.tenant_id
+    object_id = data.azurerm_client_config.current_client.object_id
+
+    key_permissions = [
+      "Create",
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Set",
+      "Get",
+      "Delete",
+      "Purge",
+      "Recover"
+    ]
+  }
+}
+
+resource "random_password" "password" {
+  length      = 20
+  min_lower   = 1
+  min_upper   = 1
+  min_numeric = 1
+  min_special = 1
+  special     = true
+}
+
+resource "azurerm_key_vault_secret" "iis_password" {
+  key_vault_id = azurerm_key_vault.terraform_kv.id
+  name         = "iispwd"
+  value        = random_password.password.result
+
+  depends_on = [ random_password.password ]
+}
+
+resource "azurerm_key_vault_secret" "nginx_password" {
+  key_vault_id = azurerm_key_vault.terraform_kv.id
+  name         = "nginxpwd"
+  value        = random_password.password.result
+
+  depends_on = [ random_password.password ]
+}
+
+
+# data "azurerm_key_vault_secret" "iis_pwd" {
+#   name         = "iispwd"
+#   key_vault_id = azurerm_key_vault.terraform_kv.id
+# }
+
+# data "azurerm_key_vault_secret" "nginx_pwd" {
+#   name         = "nginxpwd"
+#   key_vault_id = azurerm_key_vault.terraform_kv.id
+# }
--- vm.tf
+++ vm.tf
@@ -1,8 +1,8 @@
 # Create virtual machine
-resource "azurerm_windows_virtual_machine" "main" {
+resource "azurerm_windows_virtual_machine" "iis" {
   name                  = "vm-win-iis"
   admin_username        = "azureuser"
-  admin_password        = "123Quack456"
+  admin_password        = azurerm_key_vault_secret.iis_password.value
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.iis_nic.id]
@@ -25,12 +25,14 @@ resource "azurerm_windows_virtual_machine" "main" {
   boot_diagnostics {
     storage_account_uri = azurerm_storage_account.iis_account.primary_blob_endpoint
   }
+
+  depends_on = [azurerm_key_vault_secret.iis_password]
 }
 
 # Install IIS web server to the virtual machine
 resource "azurerm_virtual_machine_extension" "web_server_install" {
   name                       = "wsi-terraform-iis"
-  virtual_machine_id         = azurerm_windows_virtual_machine.main.id
+  virtual_machine_id         = azurerm_windows_virtual_machine.iis.id
   publisher                  = "Microsoft.Compute"
   type                       = "CustomScriptExtension"
   type_handler_version       = "1.8"
@@ -41,4 +43,47 @@ resource "azurerm_virtual_machine_extension" "web_server_install" {
       "commandToExecute": "powershell -ExecutionPolicy Unrestricted Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools"
     }
   SETTINGS
+}
+
+### NGINX ###
+
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "nginx" {
+  name                            = "vm-linux-nginx"
+  admin_username                  = "azureuser"
+  admin_password                  = azurerm_key_vault_secret.iis_password.value
+  location                        = azurerm_resource_group.rg.location
+  resource_group_name             = azurerm_resource_group.rg.name
+  network_interface_ids           = [azurerm_network_interface.nginx_nic.id]
+  size                            = "Standard_DS1_v2"
+  disable_password_authentication = false
+
+  os_disk {
+    name                 = "myNginxDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(<<-EOF
+              #!/bin/bash
+              apt update
+              apt install -y nginx
+              systemctl enable nginx
+              systemctl start nginx
+            EOF
+  )
+
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.iis_account.primary_blob_endpoint
+  }
+
+  depends_on = [azurerm_key_vault_secret.nginx_password]
 }
